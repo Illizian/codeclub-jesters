d4c560e736c2d37daec34dcf4b37aabc86a62467
Alex Scotton <alex@alexscotton.com>
2017-04-29 17:54:22 +0100

    Refactored regex tests to provide examples of failed cases

--- test/regex.test.js
+++ test/regex.test.js
@@ -1,62 +1,69 @@
 const regex = require('../src/regex');
 
+function enhancedTestResults(func, test, expected) {
+  let input = `${ func }(${ test }) === ${ regex[func](test) }`;
+  let output = `${ func }(${ test }) === ${ expected }`;
+
+  return expect(input).toBe(output);
+}
+
 describe('Regex', () => {
   test('1 - Return true if `string` contains numbers', () => {
-    expect(regex.containsNumber('abc123')).toBe(true);
-    expect(regex.containsNumber('abc')).toBe(false);
+    enhancedTestResults('containsNumber', 'abc123', true);
+    enhancedTestResults('containsNumber', 'abc', false);
   });
 
   test('2 - Return true if `string` contains a repeating letter', () => {
-    expect(regex.containsRepeatingLetter('bookkeeping')).toBe(true);
-    expect(regex.containsRepeatingLetter('rattler')).toBe(true);
-    expect(regex.containsRepeatingLetter('ZEPPELIN')).toBe(true);
-    expect(regex.containsRepeatingLetter('cats')).toBe(false);
-    expect(regex.containsRepeatingLetter('l33t')).toBe(false);
+    enhancedTestResults('containsRepeatingLetter', 'bookkeeping', true);
+    enhancedTestResults('containsRepeatingLetter', 'rattler', true);
+    enhancedTestResults('containsRepeatingLetter', 'ZEPPELIN', true);
+    enhancedTestResults('containsRepeatingLetter', 'cats', false);
+    enhancedTestResults('containsRepeatingLetter', 'l33t', false);
   });
 
   test('3 - Return true if `string` ends with a vowel (aeiou)', () => {
-    expect(regex.endsWithVowel('cats')).toBe(false);
-    expect(regex.endsWithVowel('gorilla')).toBe(true);
-    expect(regex.endsWithVowel('I KNOW KUNG FU')).toBe(true);
+    enhancedTestResults('endsWithVowel', 'cats', false);
+    enhancedTestResults('endsWithVowel', 'gorilla', true);
+    enhancedTestResults('endsWithVowel', 'I KNOW KUNG FU', true);
   });
 
   test('4 - Return the first series of three numbers', () => {
-    expect(regex.captureThreeNumbers('abc123')).toBe('123');
-    expect(regex.captureThreeNumbers('9876543')).toBe('987');
-    expect(regex.captureThreeNumbers('abcdef')).toBe(false);
-    expect(regex.captureThreeNumbers('12ab12ab')).toBe(false);
+    enhancedTestResults('captureThreeNumbers', 'abc123', '123');
+    enhancedTestResults('captureThreeNumbers', '9876543', '987');
+    enhancedTestResults('captureThreeNumbers', 'abcdef', false);
+    enhancedTestResults('captureThreeNumbers', '12ab12ab', false);
   });
 
   test('5 - Return true if `string` matches pattern', () => {
     // the pattern is 0XXXX XXXXXX where all X's are digits
-    expect(regex.matchesPattern('01111 123456')).toBe(true);
-    expect(regex.matchesPattern('07777 123456')).toBe(true);
-    expect(regex.matchesPattern('97777 123456')).toBe(false);
-    expect(regex.matchesPattern('47777 123456')).toBe(false);
-    expect(regex.matchesPattern('AAAAA 123456')).toBe(false);
-    expect(regex.matchesPattern('01111123456')).toBe(false);
-    expect(regex.matchesPattern('(01111) 123456')).toBe(false);
-    expect(regex.matchesPattern('(01111)123456')).toBe(false);
+    enhancedTestResults('matchesPattern', '01111 123456', true);
+    enhancedTestResults('matchesPattern', '07777 123456', true);
+    enhancedTestResults('matchesPattern', '97777 123456', false);
+    enhancedTestResults('matchesPattern', '47777 123456', false);
+    enhancedTestResults('matchesPattern', 'AAAAA 123456', false);
+    enhancedTestResults('matchesPattern', '01111123456', false);
+    enhancedTestResults('matchesPattern', '(01111) 123456', false);
+    enhancedTestResults('matchesPattern', '(01111)123456', false);
   });
 
   test('6 - Return true if `string` contains correctly-formatted monetary amounts in GBP', () => {
-    expect(regex.isGBP('£1320.03')).toBe(true);
-    expect(regex.isGBP('£132.03')).toBe(true);
-    expect(regex.isGBP('£32.03')).toBe(true);
-    expect(regex.isGBP('£2.03')).toBe(true);
-    expect(regex.isGBP('£1,023,032.03')).toBe(true);
-    expect(regex.isGBP('£20,933,209.93')).toBe(true);
-    expect(regex.isGBP('£20,933,209')).toBe(true);
-    expect(regex.isGBP('£459,049,393.21')).toBe(true);
-    expect(regex.isGBP('£1234,344.34')).toBe(false);
-    expect(regex.isGBP('34,344.34')).toBe(false);
-    expect(regex.isGBP('£,344.34')).toBe(false);
-    expect(regex.isGBP('£34,344.3')).toBe(false);
-    expect(regex.isGBP('£34,344.344')).toBe(false);
-    expect(regex.isGBP('£34,344_34')).toBe(false);
-    expect(regex.isGBP('£3,432,12.12')).toBe(false);
-    expect(regex.isGBP('£3,432,1,034.12')).toBe(false);
-    expect(regex.isGBP('4£3,432,034.12')).toBe(false);
-    expect(regex.isGBP('£2.03.45')).toBe(false);
+    enhancedTestResults('isGBP', '£1320.03', true);
+    enhancedTestResults('isGBP', '£132.03', true);
+    enhancedTestResults('isGBP', '£32.03', true);
+    enhancedTestResults('isGBP', '£2.03', true);
+    enhancedTestResults('isGBP', '£1,023,032.03', true);
+    enhancedTestResults('isGBP', '£20,933,209.93', true);
+    enhancedTestResults('isGBP', '£20,933,209', true);
+    enhancedTestResults('isGBP', '£459,049,393.21', true);
+    enhancedTestResults('isGBP', '£1234,344.34', false);
+    enhancedTestResults('isGBP', '34,344.34', false);
+    enhancedTestResults('isGBP', '£,344.34', false);
+    enhancedTestResults('isGBP', '£34,344.3', false);
+    enhancedTestResults('isGBP', '£34,344.344', false);
+    enhancedTestResults('isGBP', '£34,344_34', false);
+    enhancedTestResults('isGBP', '£3,432,12.12', false);
+    enhancedTestResults('isGBP', '£3,432,1,034.12', false);
+    enhancedTestResults('isGBP', '4£3,432,034.12', false);
+    enhancedTestResults('isGBP', '£2.03.45', false);
   });
 });
