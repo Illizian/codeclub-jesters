f89edbf34f3a10b9474b037bf206d782d1614cab
Alex Scotton <alex@alexscotton.com>
2017-05-07 14:46:35 +0100

    Update array test to fix mutability issue

--- test/arrays.test.js
+++ test/arrays.test.js
@@ -1,47 +1,42 @@
-const arrays = require('../src/arrays');
+const arrays = require('../arrays');
 
 describe('Arrays', () => {
-  var arr;
-  beforeEach(() => {
-    arr = [1,2,3,4,5,6,7,8,9,10];
-  });
-
   test('1 - Return the item from the `array` at the `index`', () => {
-    expect(arrays.getItemAtIndex(arr, 4)).toBe(5);
-    expect(arrays.getItemAtIndex(arr, 9)).toBe(10);
-    expect(arrays.getItemAtIndex(arr, 1)).toBe(2);
+    expect(arrays.getItemAtIndex([1,2,3,4,5,6,7,8,9,10], 4)).toBe(5);
+    expect(arrays.getItemAtIndex([1,2,3,4,5,6,7,8,9,10], 9)).toBe(10);
+    expect(arrays.getItemAtIndex([1,2,3,4,5,6,7,8,9,10], 1)).toBe(2);
   });
 
   test('2 - Return the index of a string as `item` from the `array`', () => {
-    expect(arrays.findItemInArray(arr, 4)).toBe(3);
-    expect(arrays.findItemInArray(arr, 8)).toBe(7);
-    expect(arrays.findItemInArray(arr, 2)).toBe(1);
+    expect(arrays.findItemInArray([1,2,3,4,5,6,7,8,9,10], 4)).toBe(3);
+    expect(arrays.findItemInArray([1,2,3,4,5,6,7,8,9,10], 8)).toBe(7);
+    expect(arrays.findItemInArray([1,2,3,4,5,6,7,8,9,10], 2)).toBe(1);
   });
 
   test('3 - Remove the item at the `index` in `array`', () => {
-    expect(arrays.removeItemAtIndexInArray(arr, 4)).toEqual([1,2,3,4,6,7,8,9,10]);
-    expect(arrays.removeItemAtIndexInArray(arr, 5)).toEqual([1,2,3,4,5,7,8,9,10]);
+    expect(arrays.removeItemAtIndexInArray([1,2,3,4,5,6,7,8,9,10], 4)).toEqual([1,2,3,4,6,7,8,9,10]);
+    expect(arrays.removeItemAtIndexInArray([1,2,3,4,5,6,7,8,9,10], 5)).toEqual([1,2,3,4,5,7,8,9,10]);
   });
 
   test('4 - Add up all the numbers in `array` and return the total', () => {
-    expect(arrays.sum(arr)).toBe(55)
-    expect(arrays.sum([...arr, 10])).toBe(65)
+    expect(arrays.sum([1,2,3,4,5,6,7,8,9,10])).toBe(55);
+    expect(arrays.sum([1,2,3,4,5,6,7,8,9,10,10])).toBe(65);
   });
 
   test('5 - Remove all numbers in `array` that are larger than `min`', () => {
-    expect(arrays.isBigger(arr, 5)).toEqual([6,7,8,9,10]);
-    expect(arrays.isBigger(arr, 1)).toEqual([2,3,4,5,6,7,8,9,10]);
-    expect(arrays.isBigger(arr.reverse(), 4)).toEqual([10,9,8,7,6,5]);
+    expect(arrays.isBigger([1,2,3,4,5,6,7,8,9,10], 5)).toEqual([6,7,8,9,10]);
+    expect(arrays.isBigger([1,2,3,4,5,6,7,8,9,10], 1)).toEqual([2,3,4,5,6,7,8,9,10]);
+    expect(arrays.isBigger([1,2,3,4,5,6,7,8,9,10].reverse(), 4)).toEqual([10,9,8,7,6,5]);
   });
 
   test('6 - Double (x2) all numbers in the `array`', () => {
-    expect(arrays.doubleItAll(arr)).toEqual([2,4,6,8,10,12,14,16,18,20]);
+    expect(arrays.doubleItAll([1,2,3,4,5,6,7,8,9,10])).toEqual([2,4,6,8,10,12,14,16,18,20]);
     expect(arrays.doubleItAll([2,4,6,8,10,12,14,16,18,20])).toEqual([4,8,12,16,20,24,28,32,36,40]);
   });
 
   test('7 - Apply the `fn` function on every item in the `array`', () => {
-    expect(arrays.applyFn(arr, num => num*2)).toEqual([2,4,6,8,10,12,14,16,18,20]);
-    expect(arrays.applyFn(arr, num => num*4)).toEqual([4,8,12,16,20,24,28,32,36,40]);
+    expect(arrays.applyFn([1,2,3,4,5,6,7,8,9,10], num => num*2)).toEqual([2,4,6,8,10,12,14,16,18,20]);
+    expect(arrays.applyFn([1,2,3,4,5,6,7,8,9,10], num => num*4)).toEqual([4,8,12,16,20,24,28,32,36,40]);
   });
 
   test('8 - Return the amount of times `item` occur in `array`', () => {
